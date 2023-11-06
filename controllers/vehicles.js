9502c60168faae85baf9073f09bf6502c8ce953a
Varunraj Rachakatla <s556300@nwmissouri.edu>
2023-11-06 09:55:08 -0600

    CSIS - 44563 - A11 - final commit

--- controllers/vehicles.js
+++ controllers/vehicles.js
@@ -15,8 +15,24 @@ exports.vehicle_detail = function (req, res) {
     res.send('NOT IMPLEMENTED: Costume detail: ' + req.params.id);
 };
 // Handle Costume create on POST.
-exports.vehicle_create_post = function (req, res) {
-    res.send('NOT IMPLEMENTED: Costume create POST');
+exports.vehicle_create_post = async function (req, res) {
+    console.log(req.body)
+    let document = new Vehicles();
+    // We are looking for a body, since POST does not have query parameters.
+    // Even though bodies can be in many different formats, we will be picky
+    // and require that it be a json object
+    // {"costume_type":"goat", "cost":12, "size":"large"}
+    document.vehicle_type = req.body.vehicle_type;
+    document.color = req.body.color;
+    document.price = req.body.price;
+    try {
+        let result = await document.save();
+        res.send(result);
+    }
+    catch (err) {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
+    }
 };
 // Handle Costume delete form on DELETE.
 exports.vehicle_delete = function (req, res) {
