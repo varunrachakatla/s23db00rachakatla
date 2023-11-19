87726f2b6b334240356882f35cde3c88fb4d85c5
Varunraj Rachakatla <s556300@nwmissouri.edu>
2023-11-19 12:50:34 -0600

    CSIS - 44563 - A13 - 3rd commit

--- routes/vehicles.js
+++ routes/vehicles.js
@@ -2,6 +2,13 @@ var express = require('express');
 var vehicle_controller = require('../controllers/vehicles');
 var router = express.Router();
 
+const secured = (req, res, next) => {
+    if (req.user) {
+        return next();
+    }
+    res.redirect("/login");
+}
+
 /* GET vehicles page. */
 router.get('/', vehicle_controller.vehicle_view_all_Page );
 
@@ -12,7 +19,7 @@ router.get('/detail', vehicle_controller.vehicle_view_one_Page);
 router.get('/create', vehicle_controller.vehicle_create_Page);
 
 /* GET create update page */
-router.get('/update', vehicle_controller.vehicle_update_Page);
+router.get('/update',secured, vehicle_controller.vehicle_update_Page);
 
 /* GET delete costume page */
 router.get('/delete', vehicle_controller.vehicle_delete_Page);
